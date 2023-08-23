fd2b7bafeed0fe8b34801ddb84e91882d6265ae6
Borball Zhai <borball.zh@gmail.com>
2023-08-23 11:45:51 -0400

    simplify the script

--- import/import.sh
+++ import/import.sh
@@ -7,15 +7,9 @@ if ! type "yq" > /dev/null; then
 fi
 
 usage(){
-  echo "Usage: $0 [spoke-cluster-kubeconfig] [spoke-cluster-kubeadmin] [spoke-cluster-password]"
-  echo "Example: $0 kubeadmin A5tmu-sy4GG-yeajX-TgfVr"
+  echo "Usage: $0"
+  echo "Example: $0"
 }
-
-if [ $# -lt 3 ]
-then
-  usage
-  exit
-fi
 
 if [[ ( $@ == "--help") ||  $@ == "-h" ]]
 then 
@@ -23,43 +17,31 @@ then
   exit
 fi
 
-kubeconfig_hub=$KUBECONFIG
-kubeconfig_spoke=$KUBECONFIG
-spoke_admin=$2
-spoke_password=$3
-
-ocs() {
-  oc --kubeconfig $kubeconfig_spoke "$@"
-}
-export -f ocs
-
-och() {
-  oc --kubeconfig $kubeconfig_hub "$@"
-}
-export -f och
-
-export cluster_name=$(ocs get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".metadata.name")
+export cluster_name=$(oc get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".metadata.name")
 export namespace=$cluster_name
-export pull_secret=$(ocs get secrets -n openshift-config pull-secret -o jsonpath={.data.\\.dockerconfigjson})
-export ssh_key=$(ocs get mc 99-master-ssh -o jsonpath={..sshAuthorizedKeys[0]})
-export domain=$(ocs get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq '.baseDomain')
-export ocp_version=$(ocs version -o json |jq -r '.openshiftVersion')
+export pull_secret=$(oc get secrets -n openshift-config pull-secret -o jsonpath={.data.\\.dockerconfigjson})
+export ssh_key=$(oc get mc 99-master-ssh -o jsonpath={..sshAuthorizedKeys[0]})
+export domain=$(oc get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq '.baseDomain')
+export ocp_version=$(oc version -o json |jq -r '.openshiftVersion')
 export imageset=img${ocp_version}-x86-64-appsub
-export api_vip=$(ocs get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".platform.baremetal.apiVIP")
-export ingress_vip=$(ocs get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".platform.baremetal.ingressVIP")
-export cluster_id=$(ocs get clusterversion version -o json | jq .spec.clusterID )
-export infra_id=$(ocs get infrastructure cluster -o json | jq .status.infrastructureName)
-export username=$(echo $spoke_admin |base64)
-export password=$(echo $spoke_password |base64 -w 0)
-export kubeconfig_secret=$(ocs get secrets -n openshift-kube-apiserver node-kubeconfigs -o jsonpath={..lb-ext\\.kubeconfig})
+
+#Need to handle case which platform is None.
+export api_vip=$(oc get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".platform.baremetal.apiVIP")
+#Need to handle case which platform is None.
+export ingress_vip=$(oc get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".platform.baremetal.ingressVIP")
+export cluster_id=$(oc get clusterversion version -o json | jq .spec.clusterID )
+export infra_id=$(oc get infrastructure cluster -o json | jq .status.infrastructureName)
+#export username=$(echo $spoke_admin |base64)
+#export password=$(echo $spoke_password |base64 -w 0)
+export kubeconfig_secret=$(oc get secrets -n openshift-kube-apiserver node-kubeconfigs -o jsonpath={..lb-ext\\.kubeconfig})
 
 jinja2 ./templates/ns.yaml.j2 > ./ns.yaml
 jinja2 ./templates/pull-secret.yaml.j2 > ./pull-secret.yaml
 jinja2 ./templates/infraenv.yaml.j2 > ./infraenv.yaml
 jinja2 ./templates/agent-cluster-install.yaml.j2 > ./agent-cluster-install.yaml
-jinja2 ./templates/kubeadmin-passwd-secret.yaml.j2 > ./kubeadmin-passwd-secret.yaml
+#jinja2 ./templates/kubeadmin-passwd-secret.yaml.j2 > ./kubeadmin-passwd-secret.yaml
 jinja2 ./templates/kubeconfig-secret.yaml.j2 > ./kubeconfig-secret.yaml
 jinja2 ./templates/cluster-deployment.yaml.j2 > ./cluster-deployment.yaml
 jinja2 ./templates/managed-cluster.yaml.j2 > ./managed-cluster.yaml
 
-och apply -k ./
+oc apply -k ./
--- scale/worker/add.sh
+++ scale/worker/add.sh
@@ -3,8 +3,10 @@
 # Add a worker and boot from discovery iso
 
 usage(){
-  echo "Usage: $0 [config.yaml]"
+  echo "Usage: $0 config.yaml [nmstate.yaml]"
+  echo "config.yaml is mandatory, nmstate.yaml is optional"
   echo "Example: $0 config-worker1.yaml"
+  echo "Example: $0 config-worker1.yaml nmstate-worker1.yaml"
 }
 
 if [ $# -lt 1 ]
@@ -19,53 +21,53 @@ then
   exit
 fi
 
+nmstate=""
 config_file=$1;
- 
-kubeconfig_hub=$KUBECONFIG
-kubeconfig_spoke=$KUBECONFIG
-
-ocs() {
-  oc --kubeconfig $kubeconfig_spoke "$@"
-}
-export -f ocs
-
-och() {
-  oc --kubeconfig $kubeconfig_hub "$@"
-}
-export -f och
+if [ $# -eq 2 ]
+then
+  nmstate=$2
+fi
 
 echo "Current cluster information"
 echo
-ocs get nodes
+oc get nodes
 echo
-ocs get clusterversion
+oc get clusterversion
 echo
 
-export cluster_name=$(yq '.cluster.name' $config_file)
+export cluster_name=$(oc get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".metadata.name")
 export namespace=$cluster_name
 
-nmstate=$(yq '.worker.nmstate // "" ' $config_file)
+#If a NMStateConfig is specified
 if [ ! -z $nmstate ]; then
   echo "Customized NMStateConfig CR provided: $nmstate"
-  och apply -f $nmstate
+  oc apply -f $nmstate
 else
-  #create NMStateConfig for static IP based on template
-  if [ "true" = "$(yq '.worker.dhcp' $config_file)" ]; then
+  #use default template
+  network=$(yq '.worker.network // "" ' $config_file)
+  if [ -z $network]; then
+    #create NMStateConfig for static IP based on template
     echo "New worker node uses DHCP, will not create NMStateConfig CR"
   else
     echo "New worker node uses static IP, will create NMStateConfig CR"
-
     jinja2 ./templates/nmstate.yaml.j2 $config_file
-    jinja2 ./templates/nmstate.yaml.j2 $config_file | och apply -f -
+    jinja2 ./templates/nmstate.yaml.j2 $config_file | oc apply -f -
   fi
-
 fi
 
 #boot the node
 bmc_address=$(yq '.worker.bmc.address' $config_file)
 bmc_username=$(yq '.master.bmc.username' $config_file)
 bmc_password=$(yq '.master.bmc.password' $config_file)
-iso_image=$(yq '.iso.address' $config_file)
+#use the assisted-image service
+iso_image=$(oc get infraenv -n $cluster -o json|jq -r '.items[0].status.isoDownloadURL')
+#Due to some bugs https://issues.redhat.com/browse/MGMT-14923, the isoDownloadURL is always pointing to the current latest OCP version(4.13 at this point).
+#Need to manually change to 4.12 to avoid issues
+iso_image=$(oc --kubeconfig $kubeconfig get infraenv -n $cluster -o json|jq -r '.items[0].status.isoDownloadURL')
+
+#fix bug
+isoDownloadURL=${isoDownloadURL//4.13/4.12}
+
 kvm_uuid=$(yq '.worker.bmc.kvm_uuid // "" ' $config_file)
 
 if [ ! -z $kvm_uuid ]; then
@@ -75,32 +77,32 @@ else
 fi
 
 #TODO: check if an older agent already existed
-until ( och get agent -n $namespace |grep -m 1 "auto-assign" ); do
+until ( oc get agent -n $namespace |grep -m 1 "auto-assign" ); do
   sleep 5
 done
 
 worker_hostname=$(yq '.worker.hostname' $config_file)
 worker_disk=$(yq '.worker.disk // "" ' $config_file)
 
-agent_name=$(och get agent -n $namespace -o jsonpath="{.items[?(@.spec.approved==false)].metadata.name}")
+agent_name=$(oc get agent -n $namespace -o jsonpath="{.items[?(@.spec.approved==false)].metadata.name}")
 
-och patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/hostname", "value": "'${worker_hostname}'" }]'
-och patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/approved", "value": true }]'
+oc patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/hostname", "value": "'${worker_hostname}'" }]'
+oc patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/approved", "value": true }]'
 sleep 10
-och patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/role", "value": "worker" }]'
+oc patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/role", "value": "worker" }]'
 
 if [ ! -z $worker_disk ]; then
-  och patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/installation_disk_id", "value": "'${worker_disk}'"}]'
+  oc patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/installation_disk_id", "value": "'${worker_disk}'"}]'
 fi
 
 #Trigger the installation
-och patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/clusterDeploymentName", "value": {"name": "'${cluster_name}'", "namespace": "'${namespace}'"} }]'
+oc patch agent -n $namespace $agent_name --type=json --patch '[{ "op": "replace", "path": "/spec/clusterDeploymentName", "value": {"name": "'${cluster_name}'", "namespace": "'${namespace}'"} }]'
 
 echo "-------------------------------"
 
 #Monitor the installation progress
-while [[ "Done" != $(och get agent -n $namespace $agent_name -o jsonpath='{..currentStage}') ]]; do
-  installationPercentage=$(och get agent -n $namespace $agent_name -o jsonpath='{..installationPercentage}')
+while [[ "Done" != $(oc get agent -n $namespace $agent_name -o jsonpath='{..currentStage}') ]]; do
+  installationPercentage=$(oc get agent -n $namespace $agent_name -o jsonpath='{..installationPercentage}')
   echo "Installation in progress: completed $installationPercentage/100"
   sleep 15
 done
@@ -108,5 +110,5 @@ done
 echo "Installation completed."
 echo
 
-ocs get nodes
+oc get nodes
 
