5d305ba12c34a2a5ba7b44db478a791c99be90d6
Borball Zhai <borball.zh@gmail.com>
2023-08-24 17:09:13 -0400

    big refactoring

---
+++ scale/scale/add-node.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+#
+# - Add a new node, can be master or worker
+# - create NMStateConfig
+# - generate discovery ISO
+# - boot the node from discovery ISO
+# - trigger the OCP deployment and monitor the installation progress
+#
+
+set -euo pipefail
+
+BASEDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+export BASEDIR=$BASEDIR
+
+usage(){
+  echo "Usage: $0 config.yaml [nm-state.yaml]"
+  echo "     config.yaml: mandatory, refer to config-sample.yaml"
+  echo "     nm-state.yaml: optional, refer to https://github.com/openshift/assisted-service/blob/master/config/crd/bases/agent-install.openshift.io_nmstateconfigs.yaml"
+  echo "Example 1: $0 config-worker1.yaml"
+  echo "Example 2: $0 config-master3.yaml nm-state-master3.yaml"
+}
+
+if [ $# -lt 1 ]
+then
+  usage
+  exit
+fi
+
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]
+then
+  usage
+  exit
+fi
+
+config_file=$1;
+nm_state=""
+if [ $# -eq 2 ]; then
+  nm_state=$2
+fi
+
+export_cluster_info(){
+  cluster_name=$(oc get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".metadata.name")
+  namespace=$cluster_name
+  export cluster_name="$cluster_name"
+}
+
+print_cluster_info(){
+  echo "-------------------------------"
+  echo "Cluster information:"
+  oc get clusterversion
+  echo
+  echo "Cluster nodes:"
+  oc get nodes
+  echo
+}
+
+create_nm(){
+  echo "-------------------------------"
+  #If a NMStateConfig is specified
+  if [ -n "$nm_state" ]; then
+    echo "Customized NMStateConfig CR provided: $nm_state"
+    cat "$nm_state"
+    oc apply -f "$nm_state"
+  else
+    #use default template
+    local network=$(yq '.node.network // "" ' "$config_file")
+    if [ -z "$network" ]; then
+      #create NMStateConfig for static IP based on template
+      echo "Customized NMStateConfig CR not provided, new node uses DHCP, will not create NMStateConfig CR."
+    else
+      echo "Customized NMStateConfig CR not provided, new node uses static IP, will create NMStateConfig CR below:"
+      jinja2 "$BASEDIR"/templates/nmstate.yaml.j2 "$config_file"
+      jinja2 "$BASEDIR"/templates/nmstate.yaml.j2 "$config_file" | oc apply -n "$namespace" -f -
+    fi
+  fi
+}
+
+boot_node(){
+  echo "-------------------------------"
+  local bmc_address=$(yq '.node.bmc.address' "$config_file")
+  local bmc_username=$(yq '.node.bmc.username' "$config_file")
+  local bmc_password=$(yq '.node.bmc.password' "$config_file")
+  local iso_image=$(oc get infraenv -n "$namespace" -o json|jq -r '.items[0].status.isoDownloadURL')
+
+  #Due to some bugs https://issues.redhat.com/browse/MGMT-14923, the isoDownloadURL is always pointing to the current latest OCP version(4.13 at this point).
+  #Need to manually change to 4.12 to avoid issues
+  iso_image=${iso_image//4.13/4.12}
+  
+  local kvm_uuid=$(yq '.node.bmc.kvm_uuid // "" ' $config_file)
+  
+  if [ -n "$kvm_uuid" ]; then
+    "$BASEDIR"/boot-from-iso.sh "$bmc_address" "$bmc_username":"$bmc_password" "$iso_image" "$kvm_uuid"
+  else
+    "$BASEDIR"/boot-from-iso.sh "$bmc_address" "$bmc_username":"$bmc_password" "$iso_image"
+  fi
+}
+
+patch_agent(){
+  echo "-------------------------------"
+
+  #TODO: check if an older agent already existed
+  until ( oc get agent -n "$namespace" 2>/dev/null |grep -m 1 "auto-assign" ); do
+    sleep 15
+  done
+
+  echo "Patching the agent to approve the node and trigger the deployment."
+  local role=$(yq '.node.role' "$config_file")
+  local hostname=$(yq '.node.hostname' "$config_file")
+  local install_disk=$(yq '.node.disk // "" ' "$config_file")
+
+  agent_name=$(oc get agent -n "$namespace" -o jsonpath="{.items[?(@.spec.approved==false)].metadata.name}")
+
+  echo "patch /spec/hostname with: ${hostname}"
+  oc patch agent -n "$namespace" "$agent_name" --type=json --patch '[{ "op": "replace", "path": "/spec/hostname", "value": "'"${hostname}"'" }]'
+  echo "patch /spec/approved with: true"
+  oc patch agent -n "$namespace" "$agent_name" --type=json --patch '[{ "op": "replace", "path": "/spec/approved", "value": true }]'
+  sleep 10
+  echo "patch /spec/role with: ${role}"
+  oc patch agent -n "$namespace" "$agent_name" --type=json --patch '[{ "op": "replace", "path": "/spec/role", "value": "'"${role}"'" }]'
+
+  if [ -n "$install_disk" ]; then
+    echo "patch /spec/installation_disk_id with ${node_disk}"
+    oc patch agent -n "$namespace" "$agent_name" --type=json --patch '[{ "op": "replace", "path": "/spec/installation_disk_id", "value": "'"${node_disk}"'"}]'
+  fi
+
+  #Trigger the installation
+  echo "patch /spec/clusterDeploymentName with {\"name\": \"${cluster_name}\", \"namespace\": \"${namespace}\"}"
+  oc patch agent -n "$namespace" "$agent_name" --type=json --patch '[{ "op": "replace", "path": "/spec/clusterDeploymentName", "value": {"name": "'"${cluster_name}"'", "namespace": "'"${namespace}"'"} }]'
+}
+
+monitor_install(){
+  echo "-------------------------------"
+  while [[ "Done" != $(oc get agent -n "$namespace" "$agent_name" -o jsonpath='{..currentStage}') ]]; do
+    local installation_percentage=$(oc get agent -n "$namespace" "$agent_name" -o jsonpath='{..installationPercentage}')
+    echo "Installation in progress: completed $installation_percentage/100"
+    sleep 45
+  done
+  echo "Installation completed."
+  echo
+}
+
+print_cluster_info
+export_cluster_info
+create_nm
+sleep 20
+boot_node
+patch_agent
+monitor_install
+print_cluster_info
+
+
+
+
---
+++ scale/scale/replace-master.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+#
+# - Replace a master with the new added one
+#
+
+set -euo pipefail
+
+BASEDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+export BASEDIR=$BASEDIR
+
+usage(){
+  echo "Usage: $0 replaced-master new-master"
+  echo "Example: $0 master0.compact.outbound.vz.bos2.lab master3.compact.outbound.vz.bos2.lab"
+}
+
+if [ $# -lt 2 ]
+then
+  usage
+  exit
+fi
+
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]
+then
+  usage
+  exit
+fi
+
+replaced_master_hostname=$1
+new_master_hostname=$2
+export new_master_hostname="$new_master_hostname"
+
+
+print_cluster_info(){
+  echo "-------------------------------"
+  echo "Cluster information:"
+  oc get clusterversion
+  echo
+  echo "Cluster nodes:"
+  oc get nodes
+  echo
+  echo "Cluster operators:"
+  oc get co
+  echo
+}
+
+export_cluster_info(){
+  local cluster_name=$(oc get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".metadata.name")
+  namespace=$cluster_name
+}
+
+create_new_bmh_machine(){
+  local infra_id=$(oc get -n "$namespace" ClusterDeployment -o jsonpath={..infraID})
+  export infra_id="$infra_id"
+  local new_hostname_short=$(echo "$new_master_hostname" |cut -d '.' -f 1)
+  export new_machine_name="$infra_id-$new_hostname_short"
+  local boot_mode=$(oc get bmh -n openshift-machine-api "$replaced_master_hostname" -o jsonpath={.spec.bootMode})
+  export boot_mode="$boot_mode"
+
+  jinja2 "$BASEDIR"/templates/baremetal-host.yaml.j2 | oc apply -f -
+  jinja2 "$BASEDIR"/templates/machine.yaml.j2 | oc apply -f -
+}
+
+link_machine_node(){
+  "$BASEDIR"/link-machine-and-node.sh "$new_machine_name" "$new_master_hostname"
+}
+
+wait_for_shutdown(){
+  # node is in ready status
+  if [ "True" = $(oc get nodes "$replaced_master_hostname" -o jsonpath="{.status.conditions[?(@.type=='Ready')].status}") ]; then
+    read -r -p "Please shut down the master node which is going to be replaced. continue if it's been down(y/n)?" choice
+    case "$choice" in
+      y|Y ) echo "yes";;
+      n|N ) echo "no";;
+      * ) echo "invalid";;
+    esac
+  fi
+}
+
+wait_etcd_operator(){
+  while [ "True" = "$(oc get co etcd -o jsonpath='{..conditions[?(@.type=="Degraded")].status}'})" ] || [ "True" = "$(oc get co etcd -o jsonpath='{..conditions[?(@.type=="Progressing")].status}'})" ] || [ "False" = "$(oc get co etcd -o jsonpath='{..conditions[?(@.type=="Available")].status}'})" ]; do
+    sleep 10
+  done
+}
+
+
+print_etcd_member(){
+  #find a healthy one
+  local etcd_pod=$(oc get pod -n openshift-etcd --selector app=etcd --field-selector status.phase=Running,metadata.name!=etcd-"$replaced_master_hostname",metadata.name!=etcd-"$new_master_hostname" -o jsonpath="{.items[0].metadata.name}")
+  
+  oc rsh -n openshift-etcd "$etcd_pod" etcdctl member list -w table
+  #local etcd_delete_member=$(oc rsh -n openshift-etcd "$etcd_pod" etcdctl member list |grep "$replaced_master_hostname" |cut -d ',' -f 1)
+  #oc rsh -n openshift-etcd "$etcd_pod" etcdctl member remove "$etcd_delete_member"
+  #oc rsh -n openshift-etcd "$etcd_pod" etcdctl member list -w table
+}
+
+delete_old_bmh_machine(){
+  echo "Delete old BaremetalHost and Machine."
+  local replaced_machine_name=$(oc get bmh -n openshift-machine-api "$replaced_master_hostname" -o jsonpath={.spec.consumerRef.name})
+  oc delete bmh -n openshift-machine-api "$replaced_master_hostname"
+  oc delete machine -n openshift-machine-api "$replaced_machine_name"
+}
+
+monitor(){
+  while [ "True" = "$(oc get co etcd -o jsonpath='{..conditions[?(@.type=="Degraded")].status}'})" ] || [ "True" = "$(oc get co etcd -o jsonpath='{..conditions[?(@.type=="Progressing")].status}'})" ] || [ "False" = "$(oc get co etcd -o jsonpath='{..conditions[?(@.type=="Available")].status}'})" ]; do
+    sleep 10
+  done
+  oc get co -w
+}
+
+
+export_cluster_info
+create_new_bmh_machine
+link_machine_node
+wait_etcd_operator
+print_etcd_member
+wait_for_shutdown
+delete_old_bmh_machine
+wait_etcd_operator
+print_etcd_member
+monitor
+print_cluster_info
