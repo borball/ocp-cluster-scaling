4aed545f5c01939c219b9b4682e9da10e88db088
Borball Zhai <borball.zh@gmail.com>
2023-09-06 15:22:52 -0400

    add cleanup script

--- cleanup/clean.sh
+++ cleanup/clean.sh
@@ -2,20 +2,19 @@
 #
 # script to clean up everything
 
-echo "You are going to detach the managed cluster from the MCE hub."
-
 BASEDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
 cluster_name=$(oc get cm -n kube-system cluster-config-v1 -o jsonpath={..install-config} |yq ".metadata.name")
 cluster_workspace="$BASEDIR"/../import/"$cluster_name"
 
-echo "Delete managed cluster: $cluster_name"
+echo "You are going to detach the managed cluster $cluster_name from the MCE hub."
+
+echo "Deleting managed cluster: $cluster_name"
 oc delete -k "$cluster_workspace"/
 
-echo "Delete MultiClusterEngine instance."
+echo "Deleting MultiClusterEngine instance."
 oc delete MultiClusterEngine multiclusterengine
 
-echo "Delete MultiClusterEngine operator."
+echo "Deleting MultiClusterEngine operator."
 oc delete ip -n multicluster-engine --all
 oc delete csv -n multicluster-engine --all
 oc delete subs -n multicluster-engine --all
